3c8e56aa3536d88a786dc6635c4cd5578fda798f
iyiola-dev <iyiola3332@gmail.com>
2020-10-31 00:31:37 +0100

    quick changes

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
-
-func main() {
-	Articles = []Article{
-		Article{
-			Title:   "Hello",
-			desc:    "the first description",
-			content: "na God help me reach here",
-		},
-	}
-	handleRequests()
-}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "welcome to the Homepage")
@@ -23,14 +13,30 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 }
 func handleRequests() {
+	http.HandleFunc("/articles", returnAllArticles)
 	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+var Articles []Article
+
 type Article struct {
 	Title   string `json:"Title"`
-	content string `json:"content"`
-	desc    string `json:"desc"`
+	Content string `json:"Content"`
+	Desc    string `json:"Desc"`
 }
 
-var Articles []Article
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint: Article has been hit")
+	json.NewEncoder(w).Encode(Articles)
+}
+func main() {
+	Articles = []Article{
+		Article{
+			Title:   "Hello",
+			Desc:    "the first description",
+			Content: "na God help me reach here",
+		},
+	}
+	handleRequests()
+}
